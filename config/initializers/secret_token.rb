2fc28bec86d11fe3f0563ad2476c8c799f037e3d
Judith Hengeveld <judithhengeveld@Macintosh.local>
2013-08-04 23:24:57 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwardsSlideshow::Application.config.secret_key_base = 'a3b56a90928e602c2e1f492b1828f66e8322ff07fa6828fbd50b91277230f049ddaa6595437776aae2ea64cb4a859e750baaf71082f5178a70f19cd95f5ad752'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AwardsSlideshow::Application.config.session_store :cookie_store, key: '_awards-slideshow_session'
